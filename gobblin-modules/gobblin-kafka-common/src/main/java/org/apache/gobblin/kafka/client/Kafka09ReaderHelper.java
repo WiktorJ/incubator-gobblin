c6bc5921b35ea3ea4e5d7e87e9db5ae165a4e270
wjurasz <wiktor.jan.jurasz@cern.ch>
2017-10-16 13:59:02 +0200

    Added licence comments to new files.

--- gobblin-modules/gobblin-kafka-common/src/main/java/org/apache/gobblin/kafka/client/Kafka09ReaderHelper.java
+++ gobblin-modules/gobblin-kafka-common/src/main/java/org/apache/gobblin/kafka/client/Kafka09ReaderHelper.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.gobblin.kafka.client;
 
 import com.google.common.base.Joiner;
@@ -10,8 +27,6 @@ import org.apache.gobblin.util.ConfigUtils;
 import java.util.Properties;
 
 import static org.apache.gobblin.kafka.writer.KafkaWriterConfigurationKeys.*;
-import static org.apache.gobblin.kafka.writer.KafkaWriterConfigurationKeys.KAFKA_SCHEMA_REGISTRY_SWITCH_NAME;
-import static org.apache.gobblin.kafka.writer.KafkaWriterConfigurationKeys.KAFKA_SCHEMA_REGISTRY_SWITCH_NAME_DEFAULT;
 
 /**
  * Created by wjurasz on 13.10.17.
